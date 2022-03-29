fd6dc1b5067cbd5fa16c68eff9ffe97a30cf196c
Maryanos <maryando78@gmail.com>
2022-03-29 15:41:48 +0100

    all files overrided by Brads from Netlify Lesson

--- app/assets/scripts/app.js
+++ app/assets/scripts/app.js
@@ -1,36 +1,20 @@
-
 import '../styles/styles.css'
 import 'lazysizes'
 import MobileMenu from './modules/MobileMenu'
 import RevealOnScroll from './modules/RevealOnScroll'
 import StickyHeader from './modules/StickyHeader'
-import HighlightedLinks from './modules/HighlightedLinks'
 
-
-//HighlightedLinks
-new HighlightedLinks()
-
-//Sticky Header
 new StickyHeader()
-
-// Reveal On Scroll Functionality
-new RevealOnScroll(document.querySelectorAll(".feature-item"), 85)
-new RevealOnScroll(document.querySelectorAll(".testimonial"), 85)
-
-
-// Mobile Menu - creating a new object that uses MobileMenu class as a blueprint
-// we can create a new object simply like this: new MobileMenu(); but we want to store this object as a variable so as below.
-new MobileMenu()
-
-
-// LOAD MODAL JS only when the button is clicked
+new RevealOnScroll(document.querySelectorAll(".feature-item"), 75)
+new RevealOnScroll(document.querySelectorAll(".testimonial"), 60)
+new MobileMenu();
 let modal
 
 document.querySelectorAll(".open-modal").forEach(el => {
   el.addEventListener("click", e => {
     e.preventDefault()
     if (typeof modal == "undefined") {
-      import(/* webpackChunkName: "modal" */'./modules/Modal').then(x => {
+      import(/* webpackChunkName: "modal" */ './modules/Modal').then(x => {
         modal = new x.default()
         setTimeout(() => modal.openTheModal(), 20)
       }).catch(() => console.log("There was a problem."))
@@ -40,8 +24,6 @@ document.querySelectorAll(".open-modal").forEach(el => {
   })
 })
 
-
-// Refreshing on the fly
 if (module.hot) {
   module.hot.accept()
 }
